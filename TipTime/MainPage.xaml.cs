2cb4da3ee2bd4fc962e28e0b4c9a1ee3e15b3e82
Sir-LaSix <saraholiveiracom41@gmail.com>
2025-09-01 22:28:29 -0300

    Aula PAM

---
+++ TipTime/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            double valor = Math.Round(ValorConta.Value);
+            double porcentagem = PorcentagemSlider;
+            double gorjeta = ValorContaEntry * (porcentagem / 100);
+
+        }
+
+        private void Gorjeta_15Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 15;
+        }
+
+        private void Gorjeta_20Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 20;
+
+        }
+
+        private void Round_DownBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Round_UpBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+        }
+
+        private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
